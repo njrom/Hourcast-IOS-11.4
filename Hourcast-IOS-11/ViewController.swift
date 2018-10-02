d3a81241df455595d722efeafc8541a97b765674
Nicholas Romano <njrom@nicholass-macbook-pro.local>
2018-10-02 16:22:09 -0400

    Initial Commit

---
+++ Hourcast-IOS-11/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hourcast-IOS-11
+//
+//  Created by Nicholas Romano on 10/2/18.
+//  Copyright © 2018 Nicholas Romano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
